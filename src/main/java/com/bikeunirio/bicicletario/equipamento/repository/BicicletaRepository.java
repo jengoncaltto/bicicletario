deb84479069e176b87b1472a5fd807bfd4ab288b
Goncaltto <goncaltosocial@gmail.com>
2025-11-05 19:01:15 -0300

    add JPArepository

--- src/main/java/com/bikeunirio/bicicletario/equipamento/repository/BicicletaRepository.java
+++ src/main/java/com/bikeunirio/bicicletario/equipamento/repository/BicicletaRepository.java
@@ -1,6 +1,8 @@
 package com.bikeunirio.bicicletario.equipamento.repository;
 
+import com.bikeunirio.bicicletario.equipamento.entity.Bicicleta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BicicletaRepository extends JpaRepository {
+public interface BicicletaRepository extends JpaRepository<Bicicleta, Integer> {
+
 }

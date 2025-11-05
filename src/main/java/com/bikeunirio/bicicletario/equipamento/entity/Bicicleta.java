bbaf6f9ac20d642e38a9f89fd84618dbfca152bd
Goncaltto <goncaltosocial@gmail.com>
2025-11-05 18:59:03 -0300

    Inserindo atributos

--- src/main/java/com/bikeunirio/bicicletario/equipamento/entity/Bicicleta.java
+++ src/main/java/com/bikeunirio/bicicletario/equipamento/entity/Bicicleta.java
@@ -4,4 +4,69 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Bicicleta {
+    Integer id;
+    String marca;
+    String modelo;
+    String ano;
+    Integer numero;
+    String status;
+
+    public Bicicleta() {
+    }
+    public Bicicleta(Integer id, String marca, String modelo, String ano, Integer numero, String status) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.numero = numero;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }

a66f3d0d328dc652ec6dfdc99d463ad5be47f6ad
Goncaltto <goncaltosocial@gmail.com>
2025-11-05 18:59:20 -0300

    Função lista todas as bicicletas

--- src/main/java/com/bikeunirio/bicicletario/equipamento/controller/BicicletaController.java
+++ src/main/java/com/bikeunirio/bicicletario/equipamento/controller/BicicletaController.java
@@ -1,8 +1,12 @@
 package com.bikeunirio.bicicletario.equipamento.controller;
 
+import com.bikeunirio.bicicletario.equipamento.entity.Bicicleta;
 import com.bikeunirio.bicicletario.equipamento.service.BicicletaService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Bicicleta")
@@ -10,4 +14,13 @@ public class BicicletaController {
 
     private BicicletaService  service;
     public BicicletaController(BicicletaService service) {}
+
+    @GetMapping
+    public List<Bicicleta> listar() {
+        List<Bicicleta> bicicletas = service.listar();
+        if(bicicletas.isEmpty()){
+            return null;
+        }
+        return bicicletas;
+    }
 }

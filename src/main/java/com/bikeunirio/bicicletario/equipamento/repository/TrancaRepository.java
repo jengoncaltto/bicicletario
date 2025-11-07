b49578738ca6edc014a045be7fa4d8d9f5cafd4a
Goncaltto <goncaltosocial@gmail.com>
2025-11-07 14:42:51 -0300

    update repository

--- src/main/java/com/bikeunirio/bicicletario/equipamento/repository/TrancaRepository.java
+++ src/main/java/com/bikeunirio/bicicletario/equipamento/repository/TrancaRepository.java
@@ -1,6 +1,7 @@
 package com.bikeunirio.bicicletario.equipamento.repository;
 
+import com.bikeunirio.bicicletario.equipamento.entity.Tranca;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrancaRepository extends JpaRepository {
+public interface TrancaRepository extends JpaRepository<Tranca, Long> {
 }

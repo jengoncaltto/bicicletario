23000eec04c53fdd669cd2a199e9bcff9cbb10ef
Goncaltto <goncaltosocial@gmail.com>
2025-11-13 04:25:25 -0300

    update EmailServiceTest.java

--- src/test/java/com/bikeunirio/bicicletario/equipamento/service/EmailServiceTest.java
+++ src/test/java/com/bikeunirio/bicicletario/equipamento/service/EmailServiceTest.java
@@ -2,12 +2,16 @@ package com.bikeunirio.bicicletario.equipamento.service;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class EmailServiceTest {
 
     @Test
     void deveEnviarEmailComSucesso() {
         EmailService service = new EmailService();
-        service.enviarEmail("teste@exemplo.com", "Assunto de Teste", "Corpo do email");
-        // Nenhuma exceção lançada = teste OK
+
+        assertDoesNotThrow(() ->
+                service.enviarEmail("teste@exemplo.com", "Assunto de Teste", "Corpo do email")
+        );
     }
 }
